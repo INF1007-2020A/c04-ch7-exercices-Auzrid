56a5d17e59af90000eab4f741e34eac455915c3a
Alexandre Cojot <alexandre.cojot@polymtl.ca>
2020-10-06 19:46:42 -0400

    n

--- exercice.py
+++ exercice.py
@@ -2,13 +2,25 @@
 # -*- coding: utf-8 -*-
 
 # TODO: Importez vos modules ici
-
+import math
+import sys
+sys.path.insert(1, r"W:\Alexandre\Ecoles\Polytechnique_2020-2021\INF1007\c04-ch6-exercices")
+from exo22 import frequence
 
 
 # TODO: Définissez vos fonction ici
+def exo1(a = 10,b = 4,c = 6,mass_volum = 67):
+    V = 4/3*(math.pi*a*b*c)
+    M = mass_volum * V
+
+    return V, M
+
 
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
+    print(exo1())
+
+    print((lambda sentence: sorted(frequence(sentence), key=frequence(sentence). __getitem__ )[-1]("ceci est un mot")))
 
     pass
